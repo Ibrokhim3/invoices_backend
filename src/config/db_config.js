f06212aad6bd7f59fa17a870f4a9a23cb27b1a3a
Ibrokhim3 <ishamsiddinov.97@gmail.com>
2023-12-07 19:34:16 +0500

    updated

--- src/config/db_config.js
+++ src/config/db_config.js
@@ -2,20 +2,20 @@ const pg = require("pg");
 
 const { Pool } = pg;
 
-// module.exports = pool = new Pool({
-//   user: process.env.PGUSER,
-//   host: process.env.PGHOST,
-//   database: process.env.PGDATABASE,
-//   password: process.env.PGPASSWORD,
-//   port: process.env.PGPORT,
-//   keepAlive: true,
-//   ssl: true,
-// });
-
 module.exports = pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "invoices",
-  password: "2626",
-  port: process.env.PORT_POSTGRES,
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+  keepAlive: true,
+  ssl: true,
 });
+
+// module.exports = pool = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "invoices",
+//   password: "2626",
+//   port: process.env.PORT_POSTGRES,
+// });
